249e473b5d0acabef74b238b5bd0165379ab5745
anga13 <kalle.spik@hotmail.com>
2019-02-07 11:22:24 +0100

    Set up Spring + dependencies

---
+++ src/main/java/se/kth/iv1201/grupp13/recruiterapplication/RecruiterApplication.java
@@ -0,0 +1,14 @@
+package se.kth.iv1201.grupp13.recruiterapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruiterApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/se/kth/iv1201/grupp13/recruiterapplication/RecruiterApplicationTests.java
@@ -0,0 +1,17 @@
+package se.kth.iv1201.grupp13.recruiterapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruiterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
