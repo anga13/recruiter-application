91cbea5233d9606ef4f68038aff067b44e24767b
Jinlei2018 <43339636+Jinlei2018@users.noreply.github.com>
2019-03-03 12:04:33 +0100

    have edit validations to the classes
    The same thing, i forgot change branch before i made the changes. So i have to push the changes directly to the main master. Since we have not much time left, you can check whether they are correct and then push merge them directly to master if you want. Sorry about that!

--- src/main/java/se/kth/iv1201/grupp13/recruiterapplication/domain/CompetenceProfile.java
+++ src/main/java/se/kth/iv1201/grupp13/recruiterapplication/domain/CompetenceProfile.java
@@ -8,106 +8,109 @@ import javax.validation.constraints.Size;
 import se.kth.iv1201.grupp13.recruiterapplication.util.Util;
 
 /**
-  * The class takes in values for how long an applicant
-  * has worked within a specific competence.
+ *   * The class takes in values for how long an applicant   * has worked within
+ * a specific competence.
  */
 @Entity
 @Table(name = "competence_profile")
 public class CompetenceProfile implements CompetenceProfileDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	@Column(name = "competence_profile_id")
-	private Long competenceProfileId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "competence_profile_id")
+    private Long competenceProfileId;
 
-	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "person_id")
-	private User user;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "person_id")
+    private User user;
 
     @NotNull(message = "{competenceProfile.competence.missing}")
-	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "competence_id")
-	private Competence competence;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "competence_id")
+    private Competence competence;
 
     @NotNull(message = "{competenceProfile.yearsOfExperience.missing}")
-    @Pattern(regexp = "^\\d{1,2}$", message = "{competenceProfile.yearsOfExperience.invalid-char}")
+    @Pattern(regexp = "^[0-9]{1,2}$", message = "{competenceProfile.yearsOfExperience.invalid-char}")
     @Size(min = 1, max = 2, message = "{competenceProfile.yearsOfExperience.length}")
-	@Column(name = "years_of_experience")
-	private int yearsOfExperience;	
-	
+    @Column(name = "years_of_experience")
+    private int yearsOfExperience;
+
     /**
      * Required by JPA, should not be used.
      */
-	protected CompetenceProfile() {
-	}
-	
+    protected CompetenceProfile() {
+    }
+
     /**
-     * <p>Creates a new instance with the specified user, competence, 
+     * <p>
+     * Creates a new instance with the specified user, competence,
      * yearsOfExperience. </p>
      *
-     * <p>A unique competence profile id will be set on the newly created
+     * <p>
+     * A unique competence profile id will be set on the newly created
      * instance.</p>
      *
-     * @param user  The competence profile's user.
+     * @param user The competence profile's user.
      * @param competence The competence profile's competence.
      * @param yearsOfExperience The competence profile's yearsOfExperience.
-     * 
+     *
      */
-    public CompetenceProfile(User user, Competence competence, int yearsOfExperience) {	       
+    public CompetenceProfile(User user, Competence competence, int yearsOfExperience) {
         this.user = user;
         this.competence = competence;
         this.yearsOfExperience = yearsOfExperience;
     }
 
+    @Override
+    public Long getCompetenceProfileId() {
+        return competenceProfileId;
+    }
 
-	@Override
-	public Long getCompetenceProfileId() {
-		return competenceProfileId;
-	}
+    @Override
+    public User getUser() {
+        return user;
+    }
 
-	@Override
-	public User getUser() {
-		return user;
-	}
+    @Override
+    public Competence getCompetence() {
+        return competence;
+    }
 
-	@Override
-	public Competence getCompetence() {
-		return competence;
-	}
+    @Override
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
 
-	@Override
-	public int getYearsOfExperience() {
-		return yearsOfExperience;
-	}
-	
     @Override
     public String toString() {
         return Util.toString(this);
     }
+
     /**
      * Sets the competence profile Id.
      */
-	public void setCompetenceProfileId(Long competenceProfileId) {
-		this.competenceProfileId = competenceProfileId;
-	}
+    public void setCompetenceProfileId(Long competenceProfileId) {
+        this.competenceProfileId = competenceProfileId;
+    }
 
     /**
      * Sets the competence profile's owner.
      */
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     /**
      * Sets the competence profile's competence.
      */
-	public void setCompetence(Competence competence) {
-		this.competence = competence;
-	}
+    public void setCompetence(Competence competence) {
+        this.competence = competence;
+    }
+
     /**
      * Sets the competence profile's years of experience.
      */
-	public void setYearsOfExperience(int yearsOfExperience) {
-		this.yearsOfExperience = yearsOfExperience;
-	}
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 }
